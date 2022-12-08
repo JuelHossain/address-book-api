ed10c8fca0964a69e5a162bf59f654516a147911
juelhossain <juelhossainnn@gmail.com>
2022-12-08 16:09:45 +0600

    server is running time to test the server

---
+++ index.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const client = require("./db/client");
+const authRouter = require("./routers/authRouter");
+const contactRouter = require("./routers/contactRouter");
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(cors());
+app.use(express.json());
+
+(async () => {
+  try {
+    await client.connect();
+    app.use("/auth", authRouter);
+    app.use("/contacts", contactRouter);
+  } catch (err) {
+    console.log("There was some error", err);
+  } finally {
+    app.get("/", (req, res) => {
+      res.send("address book server is running, Yeh!");
+    });
+    app.listen(port, () => {
+      console.log("server is running on", port);
+    });
+  }
+})();
