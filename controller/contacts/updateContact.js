2eac6f20d195a2f0b70a8256ba77c9aecf3ae755
juelhossain <juelhossainnn@gmail.com>
2022-12-08 15:38:07 +0600

    update contact api done

---
+++ controller/contacts/updateContact.js
@@ -0,0 +1,27 @@
+const { ObjectId } = require("mongodb");
+const { contactsCollection } = require("../../db/collections");
+
+const sendError = require("../../lib/sendError");
+
+// this api update a contact based on a id given by url param
+// if you don't want to update a property then don't provide that property 
+// every property given will be updated.
+
+const updateContact = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const updatedContact = req.body;
+    const filter = { _id: ObjectId(id) };
+
+    const updatedDoc = {
+      $set: updatedContact
+    };
+
+    const result = await contactsCollection.updateOne(filter, updatedDoc);
+    res.send(result);
+  } catch (err) {
+    sendError(res, err, "Updating a contact was not successfull");
+  }
+};
+
+module.exports = updateContact;
